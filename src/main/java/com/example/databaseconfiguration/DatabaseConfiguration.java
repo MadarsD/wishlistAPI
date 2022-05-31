6313d252f1b07eda28c698492690aa297c2b4157
MadarsD <madars.davids7@gmail.com>
2022-05-31 22:06:14 +0300

    database commit final

--- src/main/java/com/example/databaseconfiguration/DatabaseConfiguration.java
+++ src/main/java/com/example/databaseconfiguration/DatabaseConfiguration.java
@@ -4,11 +4,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfiguration {
+
+    private org.springframework.core.env.Environment environment;
+
+    public DatabaseConfiguration(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     @ConditionalOnProperty(prefix = "wish-list", name = "store-type", havingValue = "H2Database")
@@ -26,9 +33,9 @@ public class DatabaseConfiguration {
     public DataSource getDatabaseDataSourcePostgres() {
         return DataSourceBuilder.create()
                 .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://localhost:5432/postgres")
-                .username("postgres")
-                .password("password")
+                .url(environment.getProperty("spring.datasource.url"))
+                .username(environment.getProperty("spring.datasource.username"))
+                .password(environment.getProperty("spring.datasource.password"))
                 .build();
 
     }
